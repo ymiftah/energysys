912e1727b0216722909e9320cdfda73ed4724a61
youssefmiftah <miftahyo@outlook.fr>
2021-01-13 14:51:46 +0100

    add reserves

--- solvers/unit_commitment.py
+++ solvers/unit_commitment.py
@@ -1,5 +1,6 @@
 from solvers.economic_dispatch import QPModel
 import numpy as np
+import pandas as pd
 
 from pyomo.environ import *
 
@@ -10,6 +11,8 @@ class QPModel(object):
         self.built = False
 
     def build_model(self, system, load):
+        r = system.reserve_req
+
         m = ConcreteModel()
         m.units = Set(initialize=[u.name for u in system])
         m.t = Set(initialize=list(range(len(load))), ordered=True)
@@ -28,7 +31,8 @@ class QPModel(object):
         # Power balance
         m.eq_balance = Constraint(m.t, 
             rule=lambda m, t: sum(m.varPower[t, u] for u in m.units) >= load[t])
-    
+
+            
         # Startup Equations
         def eq_startup(m, t, u):
             if t == m.t.first():
@@ -36,6 +40,41 @@ class QPModel(object):
             else:
                 return m.binIsOn[t, u] - m.binIsOn[t-1, u] == m.binStartUp[t, u] - m.binShutDown[t, u]
         m.eq_startup = Constraint(m.t, m.units, rule=eq_startup)
+
+        def eq_min_uptime(m, t, u):
+            if system[u].min_uptime is None or t > system[u].min_uptime:
+                return Constraint.Skip
+            else:
+                return (m.binShutDown[t, u] <=
+                    1 - sum(m.binStartUp[tt, u] for tt in m.t
+                        if t - system[u].min_uptime <= tt <= t)    
+                )
+        m.eq_min_uptime = Constraint(m.t, m.units, rule=eq_min_uptime)
+
+        def eq_min_rest(m, t, u):
+            if system[u].min_rest is None or t > system[u].min_rest:
+                return Constraint.Skip
+            else:
+                return (m.binStartUp[t, u] <=
+                    1 - sum(m.binShutDown[tt, u] for tt in m.t
+                        if t - system[u].min_rest <= tt <= t)    
+                )
+        m.eq_min_rest = Constraint(m.t, m.units, rule=eq_min_rest)
+            
+        # Ramp Equations
+        def eq_ramp_up(m, t, u):
+            if  system[u].ramp_up is None or t == m.t.first():
+                return Constraint.Skip
+            else:
+                return m.varPower[t, u] - m.varPower[t-1, u] <= system[u].ramp_up * m.binIsOn[t-1,u] + system[u].min_power * m.binStartUp[t,u]
+        
+        def eq_ramp_down(m, t, u):
+            if  system[u].ramp_down is None or t == m.t.first():
+                return Constraint.Skip
+            else:
+                return m.varPower[t-1, u] - m.varPower[t, u] <= system[u].ramp_down * m.binIsOn[t,u] + system[u].min_power * m.binShutDown[t,u]
+        m.eq_ramp_up = Constraint(m.t, m.units, rule=eq_ramp_up)
+        m.eq_ramp_down = Constraint(m.t, m.units, rule=eq_ramp_down)
 
         # Min Max Power
         def eq_min_power(m, t, u):
@@ -45,6 +84,33 @@ class QPModel(object):
         m.eq_min_power = Constraint(m.t, m.units, rule=eq_min_power)
         m.eq_max_power = Constraint(m.t, m.units, rule=eq_max_power)
 
+        # Reserves
+        if 1 > r > 0:
+            m.varReserve = Var(m.t, m.units, domain=NonNegativeReals)
+            # Power reserve
+            m.eq_balance_reserve = Constraint(m.t, 
+                rule=lambda m, t: sum(m.varReserve[t,u] for u in m.units) >= load[t]* (1+r))
+            # Reserve > Power
+            m.eq_reserve_power = Constraint(m.t, m.units, 
+                rule=lambda m, t, u: m.varReserve[t,u] >= m.varPower[t,u])
+            # Reserve < Max Power
+            m.eq_reserve_max_power = Constraint(m.t, m.units, 
+                rule=lambda m, t, u: m.varReserve[t,u] <= system[u].max_power * m.binIsOn[t, u])
+            # Reserve ramp up
+            m.eq_reserve_ramp_up = Constraint(m.t, m.units, 
+                rule=lambda m, t, u: (
+                Constraint.Skip if (system[u].ramp_up is None or t == m.t.first())
+                else m.varReserve[t, u] <= m.varPower[t-1, u] + system[u].ramp_up * m.binIsOn[t-1,u] + system[u].min_power * m.binStartUp[t,u]
+                )
+            )
+            # Reserve ramp down
+            m.eq_reserve_ramp_up = Constraint(m.t, m.units, 
+                rule=lambda m, t, u: (
+                Constraint.Skip if (system[u].ramp_up is None or t == m.t.last())
+                else m.varReserve[t, u] <= system[u].min_power * m.binShutDown[t+1,u] + system[u].min_power * (m.binIsOn[t,u] - m.binShutDown[t+1,u])  
+                )
+            )
+
         # Update 
         self.m = m
         self.built = True
@@ -53,14 +119,11 @@ class QPModel(object):
     def solve(self, system, load, tee=0, force_build=False):
         if not self.built or force_build: 
             self.build_model(system, load)    
-        sol = SolverFactory("bonmin", executable="./solvers/bonmin/bonmin.exe")
-        sol.options["bonmin.allowable_gap"] = .05
-        sol.options["bonmin.time_limit"] = 200
+        sol = SolverFactory("cplex")
+        sol.options["mipgap"] = 0.01
+        sol.options["time"] = 200
         res = sol.solve(self.m, tee=tee)
-        for v in self.m.component_data_objects(Var, active=True):
-            print(v, value(v))  # doctest: +SKIP
-        print(value(self.m.cost))
-
+        return value(self.m.cost)
 
 
 class LPModel(QPModel):
@@ -74,11 +137,12 @@ class LPModel(QPModel):
 
         m.varFuelCons = Var(m.t, m.units, domain=NonNegativeReals)
         m.support_lines = ConstraintList()
+        num_lines = 2
         for t in m.t:
             for u in m.units:
-                for i in range(3):
-                    p = np.linspace(system[u].min_power, system[u].max_power, 4)
-                    fp = system[u].input_output(p)
+                p = np.linspace(system[u].min_power, system[u].max_power, num_lines+1)
+                fp = system[u].input_output(p)
+                for i in range(num_lines):
                     m.support_lines.add(
                         m.varFuelCons[t, u] / system[u].fuel_cost
                         >=
@@ -87,7 +151,7 @@ class LPModel(QPModel):
                         )
         m.cost = Objective(
             expr = sum(m.varFuelCons[t, u.name] for u in system for t in m.t)
-                   + sum((m.binIsOn[t, u.name] - 1) * u.curve[0] for u in system for t in m.t)
+                   + sum(m.binStartUp[t, u.name] * u.start_up_cost for u in system for t in m.t)
                    , sense = minimize,
         )
 
@@ -97,10 +161,15 @@ class LPModel(QPModel):
     def solve(self, system, load, tee=0, force_build=False):
         if not self.built or force_build: 
             self.build_model(system, load)    
-        sol = SolverFactory("cplex")
-        sol.options["mipgap"] = 0.01
-        sol.options["time"] = 200
+        sol = SolverFactory("cbc", executable="./solvers/cbc/cbc.exe")
+        sol.options["ratio"] = 0.01
+        sol.options["sec"] = 200
         res = sol.solve(self.m, tee=tee)
-        for v in self.m.component_data_objects(Var, active=True):
-            print(v, value(v))  # doctest: +SKIP
-        print(value(self.m.cost))
+        return value(self.m.cost)
+    
+    def get_power(self):
+        d = self.m.varPower.extract_values()
+        mux = pd.MultiIndex.from_tuples(d.keys())
+        df = pd.DataFrame(list(d.values()), index=mux).unstack(fill_value=0)
+        df.columns = df.columns.droplevel()
+        return df
--- systems/thermal.py
+++ systems/thermal.py
@@ -1,16 +1,21 @@
-from .core import Unit
+from .core import Unit, System
 import numpy as np
 
 class ThermalUnit(Unit):
     def __init__(self, name, input_output, fuel_cost,
                     min_power=-np.inf, max_power=np.inf,
-                    start_up_cost=0):
+                    start_up_cost=0, ramp_up=None, ramp_down=None,
+                    min_uptime = None, min_rest=None):
         super().__init__(name)
         self.curve = input_output
         self.min_power = min_power
         self.max_power = max_power
         self.fuel_cost = fuel_cost
         self.start_up_cost = start_up_cost
+        self.ramp_up = ramp_up
+        self.ramp_down = ramp_down
+        self.min_uptime = min_uptime
+        self.min_rest = min_rest
     
     def input_output(self, P):
         return (self.curve[0] + self.curve[1] * P + self.curve[2] * P * P)
@@ -29,4 +34,7 @@ class ThermalUnit(Unit):
 
 
 
-#class system(object):
+class UCSystem(System):
+    def __init__(self, name, units, reserve_req=0):
+        super().__init__(name, units)
+        self.reserve_req = reserve_req
